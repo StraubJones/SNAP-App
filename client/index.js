adffd5365646baf9bf548982dbd5f80aa0c42484
Christian Straubhaar-Jones <christian.straubhaar@gmail.com>
2017-02-04 11:08:36 -0600

    adds images, some basic styling, etc.

--- client/index.js
+++ client/index.js
@@ -1,7 +1,40 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
+import Paper from 'material-ui/Paper';
+import IconLocationOn from 'material-ui/svg-icons/communication/location-on';
+
+injectTapEventPlugin();
+
+//icons
+const eligibilityIcon = <FontIcon className="material-icons"
+
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <Paper zDepth={1}>
+          <img className="snap-logo" src="./images/SNAPlogo.png"></img>
+          <BottomNavigation selectedIndex={this.state.selectedIndex}>
+            <BottomNavigationItem
+              label="Eligibility"
+              icon={recentsIcon}
+              onTouchTap={() => this.select(0)}
+            />
+            <BottomNavigationItem
+              label="More Info"
+              icon={favoritesIcon}
+              onTouchTap={() => this.select(1)}
+            />
+          </BottomNavigation>
+        </Paper>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
-  <h1>Hello from React!</h1>, // JSX
+  <App />,
   document.getElementById('app')
 );
